68c4327f3b932a959f83b514e32ea8f0ced79bfc
Leandr0Fr <leandro00fr@gmail.com>
2025-01-13 14:25:13 -0300

    update config

---
+++ core/settings/local.py
@@ -0,0 +1,16 @@
+from .base import *
+
+SECRET_KEY = "soyadmin"
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+STATICFILES_DIRS = [STATIC_DIR]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": DATABASE_DIR / "db.sqlite3",
+    }
+}
